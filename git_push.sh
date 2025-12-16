be9c763ef1cfa3eeef9208d62f73040391515375
HugoSachim <hugosachim@ua.pt>
2025-12-16 19:53:43 +0000

    Auto commit

---
+++ git_push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Change to your repository directory (optional if you run inside it)
+cd /home/hogu/Desktop/savi-2025-2026-trabalho2-grupoX
+
+# Add all changes
+git add .
+
+# Commit (auto message, or blank)
+git commit -am "Auto commit"
+
+# Push to origin
+git push
+
+echo "Git push done!"
+
